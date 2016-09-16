529280480940f10574c9145f18f7fc19b66982d8
Jianhua <jianhua.wu.2014@smu.edu.sg>
2016-09-16 08:28:21 +0800

    create folder in drive

--- web/app/auth/auth.service.js
+++ web/app/auth/auth.service.js
@@ -15,9 +15,9 @@
       login: login,
       logout: logout,
       fetchAuthData: fetchAuthData,
-      fetchAuthPic: fetchAuthPic,
-      fetchAuthEmail: fetchAuthEmail,
-      fetchAuthDisplayName:fetchAuthDisplayName
+      //fetchAuthPic: fetchAuthPic,
+      //fetchAuthEmail: fetchAuthEmail,
+      //fetchAuthDisplayName:fetchAuthDisplayName
     };
 	
 	return service;
@@ -36,19 +36,33 @@
         console.log("login success");
         // The signed-in user info.
         var user = result.user;
+        $rootScope.userID = user.uid;
         
         usersRef.child(user.uid).update({
           pic: user.photoURL,
           email: user.email,
           displayName: user.displayName
         });
+        
+         var token = result.credential.accessToken;
+        // set the authentication token
+
+        gapi.auth.setToken({
+            access_token: token
+        });
+             
+        
 
         ref.child('/signinLogs/' + user.uid).set(new Date().toLocaleString("en-US"));
         
         var userData = $firebaseObject(usersRef.child(user.uid));
         //navBarService.updateNavBar(user.displayName);
         userData.$loaded().then(function(){
-           
+            //load drive API to create if have not created before
+            if(!userData.driveExcel) {
+                loadDriveApi();   
+            }
+            //Create Google Folder upon login
             $rootScope.logined = true;
             if(userData.profileLink == null) {
               $location.path('/createProfileLink');
@@ -58,7 +72,7 @@
             }
         });
       });
-  }
+    }
 
     function logout() {
       return firebase.auth().signOut();
@@ -84,6 +98,65 @@
       });
     }
 
+    function loadDriveApi() {
+      gapi.client.load('drive', 'v3', createSpread);
+    }
+    
+    function createSpread() {
+        var spreadsheetID ;
+        var folderId;
+        
+        var courseSetting = $firebaseObject(ref.child('/courseSetting'));
+        
+        courseSetting.$loaded().then(function(){
+            
+            var courseName = courseSetting.courseName;
+            var folderName = courseName + " Folder";
+            var sheetName = courseName + " Sheet";
+            
+            var folderRequest = gapi.client.drive.files.create({
+              mimeType: "application/vnd.google-apps.folder",
+              name: folderName
+            });
+
+            folderRequest.execute(function(response){
+              folderId = response.id;
+              var spreadsheetRequest = gapi.client.drive.files.create({
+                  mimeType: "application/vnd.google-apps.spreadsheet",
+                  name: sheetName,
+                  parents: [folderId]
+                });
+                //Update Firebase with folderID
+                usersRef.child($rootScope.userID).update({ driveFolder: folderId });
+
+                spreadsheetRequest.execute(function(response){
+                    spreadsheetID = response.id;
+                    //Update Firebase with folderID
+                    usersRef.child($rootScope.userID).update({ driveExcel: spreadsheetID });
+                    gapi.client.sheets.spreadsheets.batchUpdate({
+                      spreadsheetId: spreadsheetID,
+                      requests:[{
+                          updateSheetProperties:
+                          {
+                            properties:
+                            {
+                              sheetId:0,
+                              title: "Instructions"
+                            },
+                            fields: "title"
+                          }
+                        }
+                      ]
+                    }).then(function(response){
+                      appendPre('Error: ' + response.result.error.message);
+                    });
+
+                });
+            });
+        });
+      }
+      
+    /* Not in use
     function fetchAuthPic() {
       var audData = auth.$getAuth();
       if (audData) {
@@ -107,6 +180,7 @@
     		return $firebaseObject(audref.child(audData.uid).child('displayName'));
       }
     }
+    */
   }
 
 })();

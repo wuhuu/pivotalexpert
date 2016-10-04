fe4aa6aa9e036f0554d8be05c132732cf5292345
Jianhua <jianhua.wu.2014@smu.edu.sg>
2016-10-05 03:09:16 +0800

    changed achieved to full time stamp

--- web/app/lesson/lesson.controller.js
+++ web/app/lesson/lesson.controller.js
@@ -432,9 +432,8 @@
     function nextQns(chapter, question){
 
         //update course progress in firebase db
-        var dateTimeNow = new Date().toISOString().slice(0,10); 
+        var dateTimeNow = new Date().toLocaleString("en-US");
         ref.child('userProfiles').child(user.uid).child('courseProgress').child(qid).set(dateTimeNow);
-
         chapter = parseInt(chapter) - 1;
         question = parseInt(question);
 
